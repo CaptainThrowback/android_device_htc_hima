196a6db897111f098139c4b34fc01d07f04388f8
Captain Throwback <captainthrowback@hotmail.com>
2019-03-15 13:58:59 -0400

    hima: more updates for Pie decryption
    - Rearrange and update firmware files
    - Update prebuilt kernel & remove other unnecessary kernels
    - Cleanup patchlevel and vendorinit scripts

--- recovery/root/sbin/patchlevel.sh
+++ recovery/root/sbin/patchlevel.sh
@@ -2,25 +2,25 @@
 
 SCRIPTNAME="PatchLevel"
 LOGFILE=/tmp/recovery.log
-syspath="/dev/block/bootdevice/by-name/system"
 TEMPSYS=/s
 BUILDPROP=build.prop
 DEFAULTPROP=prop.default
+syspath="/dev/block/bootdevice/by-name/system"
 
 log_info()
 {
-	echo "I:$SCRIPTNAME:"$1 >> $LOGFILE
+	echo "I:$SCRIPTNAME:$1" >> "$LOGFILE"
 }
 
 log_error()
 {
-	echo "E:$SCRIPTNAME:"$1 >> $LOGFILE
+	echo "E:$SCRIPTNAME:$1" >> "$LOGFILE"
 }
 
 finish()
 {
-	umount /s
-	rmdir /s
+	umount "$TEMPSYS"
+	rmdir "$TEMPSYS"
 	setprop fde.ready 1
 	log_info "fde.ready=$(getprop fde.ready)"
 	log_info "Script complete. Device ready for decryption."
@@ -29,8 +29,8 @@ finish()
 
 finish_error()
 {
-	umount /s
-	rmdir /s
+	umount "$TEMPSYS"
+	rmdir "$TEMPSYS"
 	log_error "Script run incomplete. Device not ready for decryption."
 	exit 0
 }
@@ -67,36 +67,37 @@ if [ -f "$TEMPSYS/$BUILDPROP" ]; then
 	osver=$(grep -i 'ro.build.version.release' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
 	if [ -n "$osver" ]; then
 		resetprop ro.build.version.release "$osver"
-		sed -i "s/ro.build.version.release=.*/ro.build.version.release="$osver"/g" /$DEFAULTPROP ;
+		sed -i "s/ro.build.version.release=.*/ro.build.version.release="$osver"/g" "/$DEFAULTPROP" ;
 		log_info "New OS Version: $osver"
 	fi
 	log_info "Current security patch level: $patchlevel"
 	patchlevel=$(grep -i 'ro.build.version.security_patch' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
 	if [ -n "$patchlevel" ]; then
 		resetprop ro.build.version.security_patch "$patchlevel"
-		sed -i "s/ro.build.version.security_patch=.*/ro.build.version.security_patch="$patchlevel"/g" /$DEFAULTPROP ;
+		sed -i "s/ro.build.version.security_patch=.*/ro.build.version.security_patch="$patchlevel"/g" "/$DEFAULTPROP" ;
 		log_info "New security patch level: $patchlevel"
 	fi
 	# Set additional props from build.prop
+	# Only needed for some devices, so this section can be removed if your device isn't one of them
 	log_info "Current device: $device"
 	device=$(grep -i 'ro.product.device' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
 	if [ -n "$device" ]; then
 		resetprop ro.product.device "$device"
-		sed -i "s/ro.product.device=.*/ro.product.device="$device"/g" /$DEFAULTPROP ;
+		sed -i "s/ro.product.device=.*/ro.product.device="$device"/g" "/$DEFAULTPROP" ;
 		log_info "New device: $device"
 	fi
 	log_info "Current fingerprint: $fingerprint"
 	fingerprint=$(grep -i 'ro.build.fingerprint' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
 	if [ -n "$fingerprint" ]; then
 		resetprop ro.build.fingerprint "$fingerprint"
-		sed -i "s/ro.build.fingerprint=.*/ro.build.fingerprint="$fingerprint"/g" /$DEFAULTPROP ;
+		sed -i "s/ro.build.fingerprint=.*/ro.build.fingerprint="$fingerprint"/g" "/$DEFAULTPROP" ;
 		log_info "New fingerprint: $fingerprint"
 	fi
 	log_info "Current product: $product"
 	product=$(grep -i 'ro.build.product' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
 	if [ -n "$product" ]; then
 		resetprop ro.build.product "$product"
-		sed -i "s/ro.build.product=.*/ro.build.product="$product"/g" /$DEFAULTPROP ;
+		sed -i "s/ro.build.product=.*/ro.build.product="$product"/g" "/$DEFAULTPROP" ;
 		log_info "New product: $product"
 	fi
 	finish
@@ -108,7 +109,7 @@ else
 		log_info "Setting OS Version to $osver_orig"
 		osver=$osver_orig
 		resetprop ro.build.version.release "$osver"
-		sed -i "s/ro.build.version.release=.*/ro.build.version.release="$osver"/g" /$DEFAULTPROP ;
+		sed -i "s/ro.build.version.release=.*/ro.build.version.release="$osver"/g" "/$DEFAULTPROP" ;
 	else
 		log_info "No Original OS Version found. Proceeding with existing value."
 		log_info "Current OS version: $osver"		
@@ -119,7 +120,7 @@ else
 		log_info "Setting security patch level to $patchlevel_orig"
 		patchlevel=$patchlevel_orig
 		resetprop ro.build.version.security_patch "$patchlevel"
-		sed -i "s/ro.build.version.security_patch=.*/ro.build.version.security_patch="$patchlevel"/g" /$DEFAULTPROP ;
+		sed -i "s/ro.build.version.security_patch=.*/ro.build.version.security_patch="$patchlevel"/g" "/$DEFAULTPROP" ;
 	else
 		log_info "No Original security patch level found. Proceeding with existing value."
 		log_info "Current security patch level: $patchlevel"		
